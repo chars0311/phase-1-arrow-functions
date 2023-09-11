3613c2d5fe0b1aab548d4fb6ee4f23249e3ced77
Charlie Salcedo <salcedo.char0311@gmail.com>
2023-09-11 01:09:08 -0400

    lab done

---
+++ index.js
@@ -0,0 +1,17 @@
+// `divide` function expression
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  // Arrow function `square`
+  const square = (x) => {
+    return x * x;
+  };
+  
+  // Arrow function `add`
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  module.exports = { divide, square, add };
+  
